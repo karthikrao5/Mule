284bba24afdedd69880c3359b19f8f88b353f290
Kevin Rose <krose700@gmail.com>
2015-09-23 22:27:59 -0400

    Added MuleStore and Pub image, added moneyLabel, thePub, and muleStore to storecontroller

--- src/teamJankkk/controller/StoreController.java
+++ src/teamJankkk/controller/StoreController.java
@@ -1,7 +1,27 @@
 package teamJankkk.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import teamJankkk.Main;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by KRose on 9/23/15.
  */
-public class StoreController {
+public class StoreController extends Main implements Initializable {
+
+    @FXML
+    private ImageView thePub, muleStore;
+
+    @FXML
+    private Label moneyLabel;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
