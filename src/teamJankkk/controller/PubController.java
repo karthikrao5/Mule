4aa04af67475d00e74b4591fbfb3e90df13b05db
Kevin Rose <krose700@gmail.com>
2015-10-01 02:50:19 -0400

    Making the pub controller YOOOOO

--- src/teamJankkk/controller/PubController.java
+++ src/teamJankkk/controller/PubController.java
@@ -1,7 +1,50 @@
 package teamJankkk.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import teamJankkk.Main;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by KRose on 10/1/15.
  */
-public class PubController {
+public class PubController extends Main implements Initializable {
+
+    @FXML
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    public void pubClicked(MouseEvent event) {
+        System.out.println("Enter Pub");
+        try {
+            Pane screen3 = (Pane) FXMLLoader.load(getClass().getResource("../views/ThePub.fxml"));
+            stage.setScene(new Scene(screen3));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void goBackToMap() {
+        try {
+            Pane backToMap = (Pane) FXMLLoader.load(getClass().getResource("../views/Map1_Forest.fxml"));
+            stage.setScene(new Scene(backToMap));
+            stage.show();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
