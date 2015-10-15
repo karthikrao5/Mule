1d89914e5110f09615b330f1a6afd1f8b6e55ad7
Albert Abedi <aabedi3@gatech.edu>
2015-10-14 23:56:08 -0400

    Player.java - Added Resource instance variables and methods

--- src/teamJankkk/model/Player.java
+++ src/teamJankkk/model/Player.java
@@ -8,13 +8,18 @@ import java.util.Map;
  */
 public class Player {
     private String name, race, color;
-    private int money, score;
+    private int money, score, food, energy, smithore, crystite;
 
     public Player(String name, String race, String color) {
         this.name = name;
         this.race = race;
         this.color = color;
         this.money = 1500;
+        score = 0;
+        food = 0;
+        energy = 0;
+        smithore = 0;
+        crystite = 0;
     }
 
     public int getScore() {
@@ -41,6 +46,22 @@ public class Player {
 
     public void addMoney(int moneyToAdd) {
         this.money = money + moneyToAdd;
+    }
+
+    public void addFood(int foodToAdd) {
+        this.food = food + foodToAdd;
+    }
+
+    public void addEnergy(int energyToAdd) {
+        this.energy = energy + energyToAdd;
+    }
+
+    public void addSmithore(int smithoreToAdd) {
+        this.smithore = smithore + smithoreToAdd;
+    }
+
+    public void addCrystite(int crystiteToAdd) {
+        this.crystite = crystite + crystiteToAdd;
     }
 
     public String getName() {
